b17f664c2dc6a3b39b21aa22a74f15581722b623
William Shen <u6096655@anu.edu.au>
2017-04-05 23:34:36 +1000

    Basic tab builder functionality complete

--- js/builder.js
+++ js/builder.js
@@ -3,13 +3,14 @@
  * text, and then insert it into the popup HTML page.
  */
 
-const BUTTON = '<button class="btn-tab">';
+const INIT = '<button id="';
+const BUTTON = '" class="btn-tab">';
 const MUTED = '<i class="fa fa-volume-off" aria-hidden="true"></i>';
 const UNMUTED = '<i class="fa fa-volume-up" aria-hidden="true"></i>';
 const TITLE = '<span class="tab-title">';
 const CLOSE = '</span></button>';
 
-function getButtonHTML(isMuted, title) {
-    return BUTTON + (isMuted ? MUTED : UNMUTED) + TITLE + title + CLOSE;
+function getButtonHTML(isMuted, title, id) {
+    return INIT + id + BUTTON + (isMuted ? MUTED : UNMUTED) + TITLE + title + CLOSE;
 }
 
