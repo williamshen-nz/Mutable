726c069a119a9f585d9221cd76a8f3be8eb8a28e
manalmohania <u6019071@anu.edu.au>
2017-04-08 16:14:45 +1000

    Blacklisted websites now get their audio blocked

---
+++ js/background.js
@@ -0,0 +1,74 @@
+/*
+* Got this from http://stackoverflow.com/a/23945027/6063947
+* */
+function extractHostname(url) {
+    var hostname;
+    //find & remove protocol (http, ftp, etc.) and get the hostname
+    if (url.indexOf("://") > -1) {
+        hostname = url.split('/')[2];
+    }
+    else {
+        hostname = url.split('/')[0];
+    }
+
+    //find & remove port number
+    hostname = hostname.split(':')[0];
+
+    return hostname;
+}
+
+function getDomain(url) {
+    var domain = extractHostname(url),
+        splitArr = domain.split('.'),
+        arrLen = splitArr.length;
+
+    //extracting the root domain here
+    if (arrLen > 2) {
+        domain = splitArr[arrLen - 2] + '.' + splitArr[arrLen - 1];
+    }
+    return domain;
+}
+
+function muteTab(tab){
+    chrome.tabs.update(tab.id, {muted: true});
+    tab.mutedInfo.muted = true;
+}
+
+function muteNewTab(tab){
+    if (blockedDomains.indexOf(getDomain(tab.url)) > -1 && tab.audible) { // if the new tab's url belongs to the blocked ones
+        muteTab(tab);
+    }
+}
+
+function muteNewURL(tabId, changeInfo, tab){
+    console.log("A refresh has been detected");
+    if (blockedDomains.indexOf(getDomain(tab.url)) > -1 && tab.audible) { // if the tab's url belongs to the blocked ones
+        console.log("Tab satisifes properties");
+        muteTab(tab);
+    }
+}
+
+function begin() {
+    blockedDomains = [];
+    chrome.storage.sync.get({blockedURLs: []}, function(object){
+        for (var i = 0; i < object.blockedURLs.length; i++) {
+            blockedDomains[i] = getDomain(object.blockedURLs[i]);
+        }
+        chrome.tabs.onCreated.addListener(muteNewTab);
+        chrome.tabs.onUpdated.addListener(muteNewURL);
+    });
+
+    chrome.storage.onChanged.addListener(function(changes, areaName){
+        if(changes.blockedURLs !== undefined) {
+            console.log('Detected change in urls. The changes are ' + changes.blockedURLs.newValue);
+        }
+        chrome.storage.sync.get({blockedURLs: []}, function(obj){
+            chrome.tabs.onCreated.removeListener(muteNewTab);
+            chrome.tabs.onUpdated.removeListener(muteNewURL);
+            chrome.tabs.onCreated.addListener(muteNewTab);
+            chrome.tabs.onUpdated.addListener(muteNewURL);
+        })
+    })
+}
+
+begin();
